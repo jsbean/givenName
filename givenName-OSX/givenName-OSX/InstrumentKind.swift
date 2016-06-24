45edd5519333b541f08294d61e1a58b6c06e7858
jsbean <jbean46@gmail.com>
2016-06-24 12:59:43 +0200

    Organize

---
+++ givenName-OSX/givenName-OSX/InstrumentKind.swift
@@ -0,0 +1,45 @@
+//
+//  InstrumentKind.swift
+//  givenName-OSX
+//
+//  Created by James Bean on 6/24/16.
+//  Copyright © 2016 James Bean. All rights reserved.
+//
+
+import Staff
+
+// subset of the real InstrumentKind, for now
+
+enum InstrumentKind: String {
+    
+    static var stringValues: [String] {
+        return allCases.map { $0.rawValue }
+    }
+    
+    case flute = "Flute (C)"
+    case clarinet = "Clarinet (B flat)"
+    case saxophone = "Saxophone (Soprano)"
+    case violin = "Violin"
+    case cello = "Cello"
+    
+    var clef: StaffClefKind {
+        switch self {
+        case .flute: return .treble
+        case .clarinet: return .treble
+        case .saxophone: return .treble
+        case .violin: return .treble
+        case .cello: return .bass // maybe just use treble considering its all hight
+        }
+    }
+    
+    var transposition: Float {
+        switch self {
+        case .flute: return 0
+        case .clarinet: return 2
+        case .saxophone: return 2
+        case .violin: return 0
+        case .cello: return 0
+        }
+    }
+}
+
--- givenName-OSX/givenName-OSX/PerformerSelectionViewController.swift
+++ givenName-OSX/givenName-OSX/PerformerSelectionViewController.swift
@@ -11,38 +11,6 @@ import EnumTools
 import Color
 import Staff
 
-enum InstrumentKind: String {
-    
-    static var stringValues: [String] {
-        return allCases.map { $0.rawValue }
-    }
-    
-    case flute = "Flute (C)"
-    case clarinet = "Clarinet (B flat)"
-    case saxophone = "Saxophone (Soprano)"
-    case violin = "Violin"
-    case cello = "Cello"
-    
-    var clef: StaffClefKind {
-        switch self {
-        case .flute: return .treble
-        case .clarinet: return .treble
-        case .saxophone: return .treble
-        case .violin: return .treble
-        case .cello: return .bass // maybe just use treble considering its all hight
-        }
-    }
-    
-    var transposition: Float {
-        switch self {
-        case .flute: return 0
-        case .clarinet: return 2
-        case .saxophone: return 2
-        case .violin: return 0
-        case .cello: return 0
-        }
-    }
-}
 
 final class PerformerSelectionViewController: NSViewController {
 
