0431e7b34b91f7f8045e06bf4cb62171eb9b47b6
jsbean <jbean46@gmail.com>
2016-06-29 17:06:42 +0200

    Share model

--- givenName-OSX/givenName-OSX/PerformerSelectionViewController.swift
+++ givenName-OSX/givenName-OSX/PerformerSelectionViewController.swift
@@ -11,7 +11,6 @@ import EnumTools
 import Color
 import Staff
 
-
 final class PerformerSelectionViewController: NSViewController {
 
     @IBOutlet weak var tableView: NSTableView!
--- givenName-iOS/givenName-iOS/PerformerSelectionViewController.swift
+++ givenName-iOS/givenName-iOS/PerformerSelectionViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 
 final class PerformerSelectionViewController: UIViewController {
     
+    // performers
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     // Choose your instrument label:
     // PerformerSelectionTableView (-> InstructionsViewController)
     // givenName text label
 }
 
+extension PerformerSelectionViewController: UITableViewDataSource {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return InstrumentKind.stringValues.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+
+extension PerformerSelectionViewController: UITableViewDelegate {
+    
+    
+}
+
+// tableview delegate
+
+// tableview datasource
